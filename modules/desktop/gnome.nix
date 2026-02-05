3bae89172e7b60bbe302d91a3729a11a696ccb6c
Silas Jökel <silas@joekel.info>
2026-02-05 22:44:10 +0100

    create module for gnome based desktop

---
+++ modules/desktop/gnome.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }:
+
+{
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+    
+    xkb.layout = "de";
+  };
+  
+  services.printing.enable = true;
+
+  # Exklusive GNOME-Pakete
+  environment.systemPackages = with pkgs; [
+    gnome-tweaks
+    gnome-extensions-app
+    gnomeExtensions.appindicator
+    gnomeExtensions.dash-to-dock
+  ];
+  
+  # Audio
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+}
