bf250ef9b14d56b4b228d23053052f9095373727
Silas Jökel <silas@joekel.info>
2026-02-06 04:35:39 +0100

    allow vscode despite unfree license

--- modules/web-dev/default.nix
+++ modules/web-dev/default.nix
@@ -1,7 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   environment.systemPackages = with pkgs; [
     vscode
+  ];
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "vscode"
   ];
 }
