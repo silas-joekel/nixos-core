a845df3d0282e82c7511ad1d706c594cf8b7fc3b
Silas Jökel <silas@joekel.info>
2026-02-06 05:40:03 +0100

    add chrome to common module

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -1,11 +1,18 @@
 { pkgs, lib, ... }:
 
 {
-  # Spotify
+  # Explicitly define unfree packages
+  allowedUnfreePackages = [
+    "google-chrome"
+    "spotify"
+  ];
+
   environment.systemPackages = with pkgs; [
+    google-chrome
     spotify
   ];
-  allowedUnfreePackages = [ "spotify" ];
+
+  # Spotify specific additions
   networking.firewall.allowedTCPPorts = [ 57621 ]; # sync with mobile devices in the same network
   networking.firewall.allowedUDPPorts = [ 5353 ]; # to enable Spotify Connect, e.g. Google Cast devices
 }
---
+++ modules/gaming/default.nix
@@ -0,0 +1,11 @@
+{ pkgs, lib, ... }:
+
+{
+  # Spotify
+  #environment.systemPackages = with pkgs; [
+ #   spotify
+  #];
+  #allowedUnfreePackages = [ "spotify" ];
+  #networking.firewall.allowedTCPPorts = [ 57621 ]; # sync with mobile devices in the same network
+  #networking.firewall.allowedUDPPorts = [ 5353 ]; # to enable Spotify Connect, e.g. Google Cast devices
+}
